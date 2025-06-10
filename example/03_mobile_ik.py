b4ea28373bbb098798c321b4b3c332da4e61f8be
Noietch <yangheqing@buaa.edu.cn>
2025-06-10 13:50:18 +0000

    add obs env

--- example/03_mobile_ik.py
+++ example/03_mobile_ik.py
@@ -8,9 +8,9 @@ import viser
 import numpy as np
 from soul.robots.pcc_robot import PCCRobot
 from soul.solver import solve_ik
-from soul.collision import HalfSpace, RobotCollision, Sphere, Capsule
+from soul.collision import HalfSpace, RobotCollision, Sphere
 from soul.visualization.visualizer_viser import ViserSoftRobot
-
+from soul.envs.obs_env import ObstacleEnv
 
 DISABLE_JIT = False
 
@@ -23,26 +23,14 @@ if DISABLE_JIT:
 
 
 def main():
-    robot = PCCRobot.from_config("configs/robots/pcc_2d_mobile.json")
-    robot_coll = RobotCollision.from_config("configs/robots/pcc_2d_mobile.json")
+    robot_config = "configs/robots/pcc_2d_mobile.json"
+    map_config = "configs/maps/obstacles.json"
+
+    robot = PCCRobot.from_config(robot_config)
+    robot_coll = RobotCollision.from_config(
+        robot_config, self_collision_sampling_rate=1
+    )
     server = viser.ViserServer()
-    plane_coll = HalfSpace.from_point_and_normal(
-        np.array([0.0, 0.0, 0.0]), np.array([0.0, 0.0, 1.0])
-    )
-
-    obstacle_1 = Capsule.from_radius_height(
-        np.array([0.0, 0.0, 0.0]),
-        np.array([1]),
-        np.array([1.0, 0.0, 0.0]),
-        np.array([1, 0, 0, 0]),
-    )
-    obstacle_2 = Capsule.from_radius_height(
-        np.array([0.0, 0.0, 0.0]),
-        np.array([1]),
-        np.array([-1.0, 0.0, 0.0]),
-        np.array([1, 0, 0, 0]),
-    )
-
     robot_vis = ViserSoftRobot(server, robot_coll, root_node_name="/robot")
     ik_target_handle = server.scene.add_transform_controls(
         "/ik_target",
@@ -51,56 +39,19 @@ def main():
         wxyz=(1, 0, 0, 0),
     )
 
-    # obstacle 1
-    sphere_handle_1 = server.scene.add_transform_controls(
-        "/obstacle/obstacle_1", scale=0.8, position=(0.4, 0.3, 0.4)
-    )
-    breakpoint()
-    server.scene.add_mesh_trimesh(
-        "/obstacle/obstacle_1/mesh", mesh=obstacle_1.to_trimesh()
-    )
-
-    # obstacle 2
-    sphere_handle_2 = server.scene.add_transform_controls(
-        "/obstacle/obstacle_2", scale=0.8, position=(0.4, 0.3, 0.4)
-    )
-    server.scene.add_mesh_trimesh(
-        "/obstacle/obstacle_2/mesh", mesh=obstacle_2.to_trimesh()
-    )
-    server.scene.add_grid("/ground", width=6, height=6)
-
-    # add a slider to control the obstacle 1 position
-    obstacle_1_position_handle = server.gui.add_vector3(
-        "Obstacle 1 Position", sphere_handle_1.position, disabled=True
-    )
-    obstacle_2_position_handle = server.gui.add_vector3(
-        "Obstacle 2 Position", sphere_handle_2.position, disabled=True
-    )
-    timing_handle = server.gui.add_number("Elapsed (ms)", 0.001, disabled=True)
-    target_handle = server.gui.add_vector3(
-        "Target", ik_target_handle.position, disabled=True
-    )
+    env = ObstacleEnv(map_config)
+    env.visualize(server, editor=True)
+    collision_list = env.get_collision_list()
 
     while True:
-        capsule_coll_world_current = obstacle_1.transform_from_pos_wxyz(
-            position=np.array(sphere_handle_1.position),
-            wxyz=np.array(sphere_handle_1.wxyz),
-        )
-        capsule_coll_world_current = obstacle_2.transform_from_pos_wxyz(
-            position=np.array(sphere_handle_2.position),
-            wxyz=np.array(sphere_handle_2.wxyz),
-        )
-        start_time = time.time()
         cfg, _ = solve_ik(
             robot=robot,
             coll=robot_coll,
-            world_coll_list=[plane_coll, capsule_coll_world_current],
+            world_coll_list=collision_list,
             target_position=ik_target_handle.position,
             target_wxyz=ik_target_handle.wxyz,
         )
         pose = robot.forward_kinematics(cfg)
-        elapsed_time = time.time() - start_time
-        timing_handle.value = 0.99 * timing_handle.value + 0.01 * (elapsed_time * 1000)
         robot_vis.update_pose(pose)
 
 
--- soul/envs/obs_env.py
+++ soul/envs/obs_env.py
@@ -8,7 +8,7 @@ import time
 
 from ..robots.pcc_robot import PCCRobot, ConstantCurvatureState
 from ..collision import RobotCollision
-from ..collision import CollGeom, Sphere, Capsule
+from ..collision import CollGeom, Sphere, Capsule, HalfSpace
 from ..visualization.visualizer_plot import visualize_pcc_model_2d
 
 
@@ -21,12 +21,15 @@ class ObstacleEnv:
             self.map_config = map_config
         self.obstacle_list = self._load_map()
 
-    def _load_map(self):
+        self.obstacle_position_handle_dict = {}
+        self.obstacle_handle_dict = {}
+
+    def _load_map(self, viser=True):
         obstacle_list: list[tuple[str, CollGeom]] = []
         for name, value in self.map_config.items():
             if value["type"] == "sphere":
                 sphere = Sphere.from_center_and_radius(
-                    center=jnp.array(value["position"]),
+                    center=jnp.array([0.0, 0.0, 0.0]) if viser else jnp.array(value["position"]),
                     radius=jnp.array(value["radius"]),
                 )
                 obstacle_list.append((name, sphere))
@@ -34,11 +37,22 @@ class ObstacleEnv:
                 capsule = Capsule.from_radius_height(
                     radius=jnp.array(value["radius"]),
                     height=jnp.array(value["height"]),
-                    position=jnp.array(value["position"]),
+                    position=jnp.array([0.0, 0.0, 0.0]) if viser else jnp.array(value["position"]),
                     wxyz=jnp.array(value["wxyz"]),
                 )
                 obstacle_list.append((name, capsule))
         return obstacle_list
+
+    def get_collision_list(self):
+        obstacle_list = self._load_map(viser=False)
+        collision_list = []
+        for _, obstacle in obstacle_list:
+            collision_list.append(obstacle)
+        plane_coll = HalfSpace.from_point_and_normal(
+            jnp.array([0.0, 0.0, 0.0]), jnp.array([0.0, 0.0, 1.0])
+        )
+        collision_list.append(plane_coll)
+        return collision_list
 
     def to_merged_trimesh(self):
         # merge all obstacles into one trimesh
@@ -47,22 +61,23 @@ class ObstacleEnv:
             trimesh_list.append(obstacle.to_trimesh())
         return trimesh.util.concatenate(trimesh_list)
 
-    def visualize(self, editor: bool = False, export_path: str = None):
-        server = viser.ViserServer(port=8081)
-
+    def visualize(
+        self,
+        server: viser.ViserServer,
+        editor: bool = False,
+        export_path: str = None,
+    ):
         # create obstacle position handles
-        obstacle_position_handle_list = []
-        obstacle_handle_list = []
         if editor:
             for name, obstacle in self.obstacle_list:
                 obstacle_handle = server.scene.add_transform_controls(
                     f"/obstacle/{name}",
                     scale=0.8,
-                    position=obstacle.pose.translation(),
+                    position=jnp.array(self.map_config[name]["position"]),
                 )
                 obstacle_position_handle = server.gui.add_vector3(
                     f"{name} Position",
-                    initial_value=obstacle.pose.translation(),
+                    initial_value=jnp.array(self.map_config[name]["position"]),
                     step=0.01,
                     min=None,
                     max=None,
@@ -80,8 +95,8 @@ class ObstacleEnv:
                         obstacle_position_handle, obstacle_handle
                     )
                 )
-                obstacle_position_handle_list.append(obstacle_position_handle)
-                obstacle_handle_list.append(obstacle_handle)
+                self.obstacle_position_handle_dict[name] = obstacle_position_handle
+                self.obstacle_handle_dict[name] = obstacle_handle
 
         # add obstacles to the scenes
         for name, obstacle in self.obstacle_list:
@@ -100,20 +115,20 @@ class ObstacleEnv:
                 print("No export path provided")
                 return
             output_dict = {}
-            for obstacle in self.obstacle_list:
+            for name, obstacle in self.obstacle_list:
                 if isinstance(obstacle, Sphere):
-                    output_dict[obstacle.name] = {
+                    output_dict[name] = {
                         "type": "sphere",
-                        "position": list(obstacle.pose.translation()),
+                        "position": list(self.obstacle_handle_dict[name].position),
                         "radius": float(obstacle.radius),
                     }
                 elif isinstance(obstacle, Capsule):
-                    output_dict[obstacle.name] = {
+                    output_dict[name] = {
                         "type": "capsule",
-                        "position": list(obstacle.pose.translation()),
+                        "position": list(self.obstacle_handle_dict[name].position),
                         "radius": float(obstacle.radius),
                         "height": float(obstacle.height),
-                        "wxyz": list(obstacle.pose.rotation().as_quat()),
+                        "wxyz": list(self.obstacle_handle_dict[name].wxyz),
                     }
                 else:
                     raise ValueError(f"Unsupported obstacle type: {type(obstacle)}")
@@ -123,10 +138,11 @@ class ObstacleEnv:
 
         export_button.on_click(export_obstacle_positions)
 
+    def show(self, editor: bool = False, export_path: str = None):
+        server = viser.ViserServer(port=8081)
+        self.visualize(server, editor, export_path)
         # update obstacle position handles (Transform control -> GUI)
         while True:
-            for obstacle_position_handle, obstacle_handle in zip(
-                obstacle_position_handle_list, obstacle_handle_list
-            ):
-                obstacle_position_handle.value = obstacle_handle.position
+            for name, obstacle_handle in self.obstacle_handle_dict.items():
+                self.obstacle_position_handle_dict[name].value = obstacle_handle.position
             time.sleep(0.01)
